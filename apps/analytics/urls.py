45cb6960edef1de1969db3947b19d43b4122d4a7
Vishesh Yadav <vishesh.yadav@openlabs.co.in>
2013-10-31 15:10:48 +0530

    Web handlers to generate data for Highcharts #2738

--- apps/analytics/urls.py
+++ apps/analytics/urls.py
@@ -10,7 +10,13 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns('analytics.views',
     url(r'^$', 'home'),
 
-    url(r'^data/states/(?P<year>\d{4})$', 'demand_supply_by_state'),
-    url(r'^data/states/(?P<year>\d{4})/(?P<state_id>\d+)/$',
-            'demand_supply_in_state')
+    url(r'^demand/(?P<year>\d{4})$', 'demand'),
+    url(r'^data/demand/(?P<year>\d{4})$', 'demand_by_state'),
+    url(r'^data/demand/(?P<year>\d{4})/(?P<state_id>\d+)$',
+        'demand_in_state'),
+
+    url(r'^supply/(?P<year>\d{4})$', 'supply'),
+    url(r'^data/supply/(?P<year>\d{4})$', 'supply_by_state'),
+    url(r'^data/supply/(?P<year>\d{4})/(?P<state_id>\d+)$',
+        'supply_in_state'),
 )
--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -14,23 +14,51 @@ from django.db import connection
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-
-def get_latest_year():
-    return '2013'
+from analytics.models import State, City, DemandData, SupplyBase
+from admin.models import SubSector
 
 
 def home(request):
-    "Main analytics page"
-    year = int(get_latest_year())
+    "Home page. What to show?"
+    demand_years = sorted([
+        k['year'] for k in DemandData.objects.values('year').distinct()
+    ], reverse=True)
+    supply_years = sorted([
+        k['year'] for k in SupplyBase.objects.values('year').distinct()
+    ], reverse=True)
+
     c = Context({
-        'analytics_year': year
+        'analytics_demand_years': demand_years,
+        'analytics_supply_years': supply_years
     })
     return render_to_response('analytics.html', c,
             context_instance=RequestContext(request))
 
 
-def demand_supply_by_state(request, year):
-    "Returns a list of states"
+def demand(request, year):
+    "Main demand page"
+    year = int(year)
+    c = Context({
+        'analytics_year': year
+    })
+    return render_to_response('demand.html', c,
+            context_instance=RequestContext(request))
+
+
+def supply(request, year):
+    "Main supply page"
+    year = int(year)
+    c = Context({
+        'analytics_year': year
+    })
+    return render_to_response('supply.html', c,
+            context_instance=RequestContext(request))
+
+
+def demand_by_state(request, year):
+    "Returns a list of states and their demand"
+    year = int(year)
+
     cursor = connection.cursor()
     cursor.execute('''
         SELECT DemandT.year AS year,
@@ -40,10 +68,10 @@ def demand_supply_by_state(request, year):
         FROM analytics_demanddata AS DemandT,
              analytics_state AS StateT,
              analytics_city AS CityT
-        WHERE year = %d AND CityT.id = DemandT.city_id
+        WHERE year = %s AND CityT.id = DemandT.city_id
                         AND CityT.state_id = StateT.id
         GROUP BY state;
-    ''' % int(year))
+    ''', (year,))
 
     states = {}
     demand_data = {}
@@ -52,73 +80,310 @@ def demand_supply_by_state(request, year):
         demand_data[state] = demand
         states[state] = state_id
 
+    return HttpResponse(json.dumps({
+        'year': year,
+        'states': states,
+        'data': demand_data,
+    }), content_type='text/json')
+
+
+def demand_in_state(request, year, state_id):
+    "Returns complete JSON data to drilldown in given state"
+    year = int(year)
+    state_id = int(state_id)
+    state_name = State.objects.get(pk=state_id).name
+
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT
+            DemandT.city_id AS city_id,
+            CityT.name AS city,
+            sum(demand) AS demand
+        FROM
+            analytics_demanddata AS DemandT,
+            analytics_city AS CityT,
+            analytics_state AS StateT
+        WHERE
+            DemandT.year = %s AND
+            StateT.id = %s AND
+            CityT.state_id = StateT.id AND
+            DemandT.city_id = CityT.id
+        GROUP BY city_id;
+    ''', (year, state_id))
+
+    cities = []
+    data = []
+
+    for city_id, city, demand in cursor.fetchall():
+        cities.append(city)
+        data.append({
+            'y': int(demand),
+            'drilldown': _demand_subsector(year, int(city_id))
+        })
+
+    json_data = {
+        'name': "Demand in %s (%d)" % (state_name, year),
+        'categories': cities,
+        'data': data,
+    }
+
+    return HttpResponse(json.dumps({
+        'year': year,
+        'state': state_name,
+        'data': json_data,
+    }), content_type='text/json')
+
+
+def _demand_subsector(year, city_id):
+    "Return drilldown JSON data for given subsector"
+
+    city = City.objects.get(pk=city_id)
+    city_name = city.name
+    state_name = city.state.name
+
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT
+            SubSectorT.id AS subsector_id,
+            SubSectorT.name AS subsector,
+            sum(demand) AS demand
+        FROM
+            analytics_demanddata AS DemandT,
+            admin_subsector AS SubSectorT,
+            admin_occupation AS OccupationT
+        WHERE
+            DemandT.year = %s AND
+            DemandT.city_id = %s AND
+            OccupationT.id = DemandT.occupation_id AND
+            SubSectorT.id = OccupationT.sub_sector_id
+        GROUP BY subsector_id;
+    ''', (year, city_id))
+
+    subsectors = []
+    data = []
+
+    for subsector_id, subsector, demand in cursor.fetchall():
+        subsectors.append(subsector)
+        data.append({
+            'y': int(demand),
+            'drilldown': _demand_occupation(year, city_id, int(subsector_id))
+        })
+
+    json_data = {
+        'name': "Demand in %s, %s (%d) by Sub Sector" %
+                        (city_name, state_name, year),
+        'categories': subsectors,
+        'data': data,
+    }
+    return json_data
+
+
+def _demand_occupation(year, city_id, subsector_id):
+    "Return drilldown JSON data for given subsector"
+
+    city = City.objects.get(pk=city_id)
+    city_name = city.name
+    state_name = city.state.name
+    subsector_name = SubSector.objects.get(pk=subsector_id).name
+
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT
+            DemandT.occupation_id AS occupation_id,
+            OccupationT.name AS occupation,
+            sum(demand) AS demand
+        FROM
+            analytics_demanddata AS DemandT,
+            admin_subsector AS SubSectorT,
+            admin_occupation AS OccupationT
+        WHERE
+            DemandT.year = %s AND
+            DemandT.city_id = %s AND
+            SubSectorT.id = %s AND
+            OccupationT.id = DemandT.occupation_id
+        GROUP BY occupation_id;
+    ''', (year, city_id, subsector_id))
+
+    occupations = []
+    data = []
+
+    for occupation_id, occupation, demand in cursor.fetchall():
+        occupations.append(occupation)
+        data.append({
+            'y': int(demand)
+        })
+
+    json_data = {
+        'name': "Demand in %s, %s (%d) by Occupations for %s" %
+                        (city_name, state_name, year, subsector_name),
+        'categories': occupations,
+        'data': data,
+    }
+    return json_data
+
+
+def supply_by_state(request, year):
+    "Returns a list of states and their supply"
+    year = int(year)
+
+    cursor = connection.cursor()
     cursor.execute('''
         SELECT SupplyT.year AS year,
+            StateT.id AS state_id,
             StateT.name AS state,
             sum(SupplyT.supply) AS supply
         FROM analytics_supplybase AS SupplyT,
             analytics_state AS StateT,
             analytics_city AS CityT
-        WHERE year = %d AND CityT.id = SupplyT.city_id
+        WHERE year = %s AND CityT.id = SupplyT.city_id
                         AND CityT.state_id = StateT.id
         GROUP BY state;
-    ''' % int(year))
+    ''', (year,))
 
+    states = {}
     supply_data = {}
-    for _, state, supply in cursor.fetchall():
+    for _, state_id, state, supply in cursor.fetchall():
         supply = int(supply)
         supply_data[state] = supply
+        states[state] = state_id
 
     return HttpResponse(json.dumps({
         'year': year,
         'states': states,
-        'demand_data': demand_data,
-        'supply_data': supply_data,
-    }))
+        'data': supply_data,
+    }), content_type='text/json')
 
 
-def demand_supply_in_state(request, year, state_id):
-    "Returns a list of states"
+def supply_in_state(request, year, state_id):
+    "Returns complete JSON data to drilldown in given state"
+    year = int(year)
+    state_id = int(state_id)
+    state_name = State.objects.get(pk=state_id).name
+
     cursor = connection.cursor()
     cursor.execute('''
-        SELECT DemandT.year AS year,
-           CityT.name AS city,
-           CityT.id AS city_id,
-           sum(DemandT.demand) AS demand
-        FROM analytics_demanddata AS DemandT,
-             analytics_city AS CityT
-        WHERE year = '%d' AND CityT.state_id = '%d'
-                          AND CityT.id = DemandT.city_id
-        GROUP BY DemandT.city_id;
-    ''' % (int(year), int(state_id),))
+        SELECT
+            SupplyT.city_id AS city_id,
+            CityT.name AS city,
+            sum(supply) AS supply
+        FROM
+            analytics_supplybase AS SupplyT,
+            analytics_city AS CityT,
+            analytics_state AS StateT
+        WHERE
+            SupplyT.year = %s AND
+            StateT.id = %s AND
+            CityT.state_id = StateT.id AND
+            SupplyT.city_id = CityT.id
+        GROUP BY city_id;
+    ''', (year, state_id))
 
-    cities = {}
-    demand_data = {}
-    for _, city, city_id, demand in cursor.fetchall():
-        demand = int(demand)
-        demand_data[city] = demand
-        cities[city] = city_id
+    cities = []
+    data = []
 
-    cursor.execute('''
-        SELECT DemandT.year AS year,
-           CityT.name AS city,
-           sum(DemandT.demand) AS demand
-        FROM analytics_demanddata AS DemandT,
-             analytics_city AS CityT
-        WHERE year = '%d' AND CityT.state_id = '%d'
-                          AND CityT.id = DemandT.city_id
-        GROUP BY DemandT.city_id;
-    ''' % (int(year), int(state_id),))
+    for city_id, city, supply in cursor.fetchall():
+        cities.append(city)
+        data.append({
+            'y': int(supply),
+            'drilldown': _supply_subsector(year, int(city_id))
+        })
 
-    supply_data = {}
-    for _, city, supply in cursor.fetchall():
-        supply = int(supply)
-        supply_data[city] = supply
+    json_data = {
+        'name': "Supply in %s (%d)" % (state_name, year),
+        'categories': cities,
+        'data': data,
+    }
 
     return HttpResponse(json.dumps({
         'year': year,
-        'state_id': state_id,
-        'cities': cities,
-        'demand_data': demand_data,
-        'supply_data': supply_data,
-    }))
+        'state': state_name,
+        'data': json_data,
+    }), content_type='text/json')
+
+
+def _supply_subsector(year, city_id):
+    "Return drilldown JSON data for given subsector"
+
+    city = City.objects.get(pk=city_id)
+    city_name = city.name
+    state_name = city.state.name
+
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT
+            SubSectorT.id AS subsector_id,
+            SubSectorT.name AS subsector,
+            sum(supply) AS supply
+        FROM
+            analytics_supplybase AS SupplyT,
+            admin_subsector AS SubSectorT,
+            admin_occupation AS OccupationT
+        WHERE
+            SupplyT.year = %s AND
+            SupplyT.city_id = %s AND
+            OccupationT.id = SupplyT.occupation_id AND
+            SubSectorT.id = OccupationT.sub_sector_id
+        GROUP BY subsector_id;
+    ''', (year, city_id))
+
+    subsectors = []
+    data = []
+
+    for subsector_id, subsector, supply in cursor.fetchall():
+        subsectors.append(subsector)
+        data.append({
+            'y': int(supply),
+            'drilldown': _supply_occupation(year, city_id, int(subsector_id))
+        })
+
+    json_data = {
+        'name': "Supply in %s, %s (%d) by Sub Sector" %
+                        (city_name, state_name, year),
+        'categories': subsectors,
+        'data': data,
+    }
+    return json_data
+
+
+def _supply_occupation(year, city_id, subsector_id):
+    "Return drilldown JSON data for given subsector"
+
+    city = City.objects.get(pk=city_id)
+    city_name = city.name
+    state_name = city.state.name
+    subsector_name = SubSector.objects.get(pk=subsector_id).name
+
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT
+            SupplyT.occupation_id AS occupation_id,
+            OccupationT.name AS occupation,
+            sum(supply) AS supply
+        FROM
+            analytics_supplybase AS SupplyT,
+            admin_subsector AS SubSectorT,
+            admin_occupation AS OccupationT
+        WHERE
+            SupplyT.year = %s AND
+            SupplyT.city_id = %s AND
+            SubSectorT.id = %s AND
+            OccupationT.id = SupplyT.occupation_id
+        GROUP BY occupation_id;
+    ''', (year, city_id, subsector_id))
+
+    occupations = []
+    data = []
+
+    for occupation_id, occupation, supply in cursor.fetchall():
+        occupations.append(occupation)
+        data.append({
+            'y': int(supply)
+        })
+
+    json_data = {
+        'name': "Supply in %s, %s (%d) by Occupations for %s" %
+                        (city_name, state_name, year, subsector_name),
+        'categories': occupations,
+        'data': data,
+    }
+    return json_data

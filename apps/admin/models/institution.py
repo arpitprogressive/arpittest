bd75d647c633249300621a82c7cf603236101df5
Vishesh Yadav <vishesh.yadav@openlabs.co.in>
2013-11-28 14:23:33 +0530

    [Analytics][Supply-1] Where are supply side institutions located #3241

--- apps/admin/models/institution.py
+++ apps/admin/models/institution.py
@@ -21,11 +21,24 @@ class Institution(models.Model):
         '''
         app_label = 'admin'
 
+    UNIVERSITY_CHOICES = (
+        ('CU', 'Central University'),
+        ('SU', 'State University'),
+        ('DU', 'Deemed University'),
+        ('PU', 'Public University'),
+        ('INI', 'Institute of National Importance'),
+    )
+
     name = models.CharField(
         max_length=100, default=None, unique=True, db_index=True,
     )
     url = models.URLField(max_length=100, unique=True)
     international = models.BooleanField(default=False)
+    city = models.ForeignKey('analytics.City', null=True, blank=True)
+    is_university = models.BooleanField()
+    university_type = models.CharField(
+        max_length=3, choices=UNIVERSITY_CHOICES, null=True, blank=True
+    )
 
     def __unicode__(self):
         '''
@@ -38,7 +51,7 @@ class InstitutionAdmin(admin.ModelAdmin):
     '''
         Institution view for admin
     '''
-    list_display = ('name', 'url', 'international')
+    list_display = ('name', 'url', 'international', )
     list_filter = ('international',)
     list_per_page = 20
     search_field = ['name', 'url']

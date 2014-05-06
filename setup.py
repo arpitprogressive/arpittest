fc38f45344893ac2b1a66480210d2f55561cd8fb
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-05-06 15:11:56 +0530

    Release version 1.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Pursuite',
-    version='1.4',
+    version='1.5',
     description='Django - Pursuite',
     author='Openlabs Technologies & Consulting (P) Limited',
     author_email='info@openlabs.co.in',
